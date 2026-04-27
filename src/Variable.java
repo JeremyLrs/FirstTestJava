2a102d00262dac60b715dc6f8bd074ca87dcab9d
JeremyLrs <hello@laurensjeremy.com>
2026-04-27 18:22:17 +0200

    feat print variable

---
+++ src/Variable.java
@@ -0,0 +1,9 @@
+public class Variable {
+    public static void main(String[] args) {
+        char c = 'a';
+        int age = 20;
+        double moyenne = 14.2;
+
+        System.out.println("Lettre : " + c + "\nAge : " + age + "\nMoyenne : " + moyenne);
+    }
+}
